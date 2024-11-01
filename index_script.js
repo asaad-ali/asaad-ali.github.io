de0513bd6b086e78b398a6df3949821825990c0b
asaad-ali <asaad.ca@hotmail.com>
2024-11-01 07:15:22 -0400

    Add files via upload

---
+++ index_script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const home_button = document.getElementById('home-button');
+    home_button.addEventListener('click', function() {
+        window.location.href = 'index.html';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const updates_button = document.getElementById('updates-button');
+    updates_button.addEventListener('click', function() {
+        window.location.href = 'updates.html';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const about_button = document.getElementById('about-button');
+    about_button.addEventListener('click', function() {
+        window.location.href = 'about.html';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const contact_button = document.getElementById('contact-button');
+    contact_button.addEventListener('click', function() {
+        window.location.href = 'contact.html';
+    });
+});
