4c9d11f0e719df1547656f5a685a9673572ef238
Asaad <asaad.ca@hotmail.com>
2024-11-16 23:14:53 -0500

    Add files via upload

---
+++ scripts/copyright.js
@@ -0,0 +1,19 @@
+/**
+ * @description
+ * Targets `element id: "copyright-string"` and sets `.textContent`.
+ * @returns {null} null
+ */
+export function copyright() {
+
+    const YEAR = "2024-" + (((new Date()).getFullYear()) + 1);
+    const NAME = "Asaad Ali";
+    const NOTICE = "All rights reserved."
+
+    // Structure copyright string.
+    const FINAL_STRING = YEAR + " " + NAME + ". " + NOTICE;
+
+    // Set copyright string.
+    document.getElementById("copyright-string").textContent = FINAL_STRING;
+
+    return null;
+}
