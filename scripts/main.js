6110d5b493442bd416766ae5a5d6081add9e10ca
Asaad <asaad.ca@hotmail.com>
2025-05-26 10:32:34 -0400

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,5 @@
 import { nav_menu } from "./nav_menu.js?v=0.0.0";
 import { copyright } from "./copyright.js?v=0.0.0"
-import { cursor_tail } from "../animations/cursor_tail/cursor_tail.js?v=0.0.0"
 
 nav_menu();
 copyright();
-cursor_tail();
