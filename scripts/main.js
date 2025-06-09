f65980f20255da273af83379d4e3de36b8da3c5b
Asaad <asaad.ca@hotmail.com>
2025-06-09 06:58:13 -0400

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-import { nav_menu } from "./nav_menu.js?v=0.0.0";
-import { copyright } from "./copyright.js?v=0.0.0";
+import { nav_menu } from "/scripts/nav_menu.js?v=0.0.0";
+import { copyright } from "/scripts/copyright.js?v=0.0.0";
 
 nav_menu();
 copyright();
